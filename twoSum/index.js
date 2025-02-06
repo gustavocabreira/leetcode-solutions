c5b3d9a01f0f2101f7c33880a1b1a002a561cd17
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-06 13:48:54 -0300

    two sum

---
+++ twoSum/index.js
@@ -0,0 +1,11 @@
+const twoSum = (nums, target) => {
+  let storage = {};
+
+  for (let [index, num] of nums.entries()) {
+    if (storage[num] !== undefined) return [storage[num], index];
+   
+    storage[target - num] = index;
+  }
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
